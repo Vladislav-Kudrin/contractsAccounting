02d2908272417cb0579a7488894932624a02fb47
Vladislav <vladislav2001vlad26@gmail.com>
2021-06-07 17:28:22 +0300

    Added DBConfig.java file.

---
+++ src/sample/DBConfig.java
@@ -0,0 +1,15 @@
+package sample;
+
+class DBConfig {
+    static final String HOST = "*";
+    static final String PORT = "*";
+    static final String USER = "*";
+    static final String PASSWORD = "*";
+    static final String DATABASE = "contracts_accounting";
+    static final String CONTRACTS = "contracts";
+    static final String DATE = "date";
+    static final String NUMBER = "number";
+    static final String PATH = "path";
+    static final String DESCRIPTION = "description";
+    static final String STATUS = "status";
+}
