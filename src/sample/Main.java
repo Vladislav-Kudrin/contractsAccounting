164591c91fecffd4662a3acb91aa953f8c9276f8
Vladislav <vladislav2001vlad26@gmail.com>
2021-06-19 21:47:31 +0300

    Increased window's size.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Учет договоров");
-        primaryStage.setScene(new Scene(root, 598, 581));
+        primaryStage.setScene(new Scene(root, 726, 581));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
